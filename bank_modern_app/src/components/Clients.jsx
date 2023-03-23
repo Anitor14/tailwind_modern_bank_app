9d9e2ffce1b32b784fd46bff56754922eef2c32a
Anitor <anitorabraham@gmail.com>
2023-03-23 22:02:42 +0100

    done with the clients section

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { clients } from "../constants";
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <section id="clients" className="flex justify-center items-center my-4">
+      <div className="flex justify-center items-center flex-wrap w-full">
+        {clients.map((client) => {
+          return (
+            <div className="flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px]">
+              <img
+                src={client.logo}
+                alt="client"
+                className="sm:w-[192px] w-[100px] object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
