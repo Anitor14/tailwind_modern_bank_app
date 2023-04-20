6f5994de5e54a33a9dc58f5cd35cc69b40fb14ed
Anitor <anitorabraham@gmail.com>
2023-04-20 14:11:39 +0100

    changed to flexCenter

--- bank_modern_app/src/components/CTA.jsx
+++ bank_modern_app/src/components/CTA.jsx
@@ -3,9 +3,11 @@ import Button from "./Button";
 
 const CTA = () => {
   return (
-    <section className="flex justify-center items-center sm:my-16 my-6 sm:px-16 px-6 sm:py-12 py-4 sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow">
+    <section
+      className={`${styles.flexCenter} sm:my-16 my-6 sm:px-16 px-6 sm:py-12 py-4 sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
       <div className="flex-1 flex flex-col">
-        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <h2 className={styles.heading2}>Let's try our service now</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Everything you need to accept card payments and grow your business
           anywhere on the planet
